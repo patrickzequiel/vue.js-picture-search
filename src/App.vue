6b13f75280077b68431ed61b8fcf9bd7cc3277fe
Patrick Cruz <patrickezequielscruz@gmail.com>
2022-03-08 19:53:36 +0100

    testing

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="container">
     <h1>{{ titulo }}</h1>
-    <ul>
+    <ul class="card-container">
       <li v-for="digimon in digimons">
-        <card>
-          {{ digimon.name }}
-          <img :src="digimon.img" alt="" />
+        <card class="card">
+          <h1 class="card-text">{{ digimon.name }}</h1>
+          <img :src="digimon.img" alt="" class="card-image" />
+          <span class="card-stats">
+            {{ digimon.level }}
+          </span>
         </card>
       </li>
     </ul>
@@ -22,7 +25,7 @@ components: {
 export default {
   data() {
     return {
-      titulo: "Pokemon Pic",
+      titulo: "Digiman Card",
       digimons: this.digimons
     };
   },
@@ -41,8 +44,69 @@ export default {
 
 <style>
 .container {
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  gap: 10px;
   align-items: center;
+  padding-left: 80px;
+  padding-right: 80px;
+  justify-content: center;
+  align-items: center;
+  justify-items: center;
+}
+
+.card-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 50px;
+  list-style: none;
+}
+
+.card {
+  display: grid;
+  grid-template-columns: 300px;
+  grid-template-rows: 250px 100px 80px;
+  grid-template-areas: "image" "text" "stats";
+  background: #ffffff;
+  border-radius: 18px;
+  width: 300px;
+  align-items: center;
+  justify-items: center;
+  box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.6);
+
+  transition: 0.2s;
+}
+
+.card:hover {
+  transform: scale(1.15);
+  box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.6);
+}
+
+.card .card-text {
+  color: rgb(0, 0, 0);
+  font-size: 50px;
+  font-weight: 300;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-self: flex-end;
+}
+
+.card .card-image {
+  grid-area: image;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+  background-size: cover;
+  height: 200px;
+}
+
+.card .card-stats {
+  grid-area: stats;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 30px;
+  color: rgb(133, 133, 133);
 }
 </style>
